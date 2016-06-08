8d17f7231cd20937972c63ca25093094b40c04d1
Piotr Jagielski <pjg@touk.pl>
2016-06-08 23:08:05 +0200

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjagielski/disclojure "0.1.1-SNAPSHOT"
+(defproject pjagielski/disclojure "0.1.1"
   :description "A live coding environment for Overtone and Leipzig"
   :url "https://github.com/pjagielski/disclojure"
   :license {:name "Eclipse Public License"
